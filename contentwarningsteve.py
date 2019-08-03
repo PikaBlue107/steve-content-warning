e9abf0bdb2472bdf967c14798693f29d3a25e261
PikaBlue107 <jvgriese@ncsu.edu>
2019-08-03 03:32:06 -0400

    0.3 Create functionality for scanning filter words and creating filter message to send to chat.

--- contentwarningsteve.py
+++ contentwarningsteve.py
@@ -3,21 +3,63 @@ from discord.ext import commands
 from discord import client
 import json
 
-with open("auth.json", "r") as auth_file:
+
+#Load 
+try:
+	auth_file = open("auth.json", "r")
 	auth_string = json.load(auth_file)["token"]
-with open("package.json", "r") as data_file:
+except FileNotFoundError:
+	print("Authorization file does not exist. Exiting...")
+	exit()
+except ValueError:
+	print("auth.json is corrupted. Retrieve a new auth.json file, then relaunch. Exiting...")
+	exit()
+finally:
+	auth_file.close()
+
+try:
+	package_file = open("package.json", "r")
+	package = json.load(package_file)
+except FileNotFoundError:
+	print("package.json file does not exist. Exiting...")
+	exit()
+except ValueError:
+	print("package.json is corrupted. Retreive a new package.json file, then relaunch. Exiting...")
+	exit()
+finally:
+	package_file.close()
+
+try:
+	data_file = open("data.json", "r")
 	data = json.load(data_file)
+except FileNotFoundError:
+	print("No data file found. Creating new file...")
+	data = {
+		"filters" : ()
+	}
+	with open("data.json", "w") as data_file:
+		json.dump(data, data_file)
+	print("Data file created.")
+except ValueError:
+	print("data.json is corrupted. Please fix the file manually, or delete it to restart with a fresh data file. Exiting...")
+	exit()
+finally:
+	data_file.close()
+
+print(data)
+
 #TODO exit if auth.json or package.json not found
-BOT_NAME = data["name"]
-BOT_VERSION = data["version"]
-BOT_DESC = data["description"]
-BOT_MAIN = data["main"]
-BOT_AUTHOR = data["author"]
-BOT_DEPENDENCIES = data["dependencies"]
+BOT_NAME = package["name"]
+BOT_VERSION = package["version"]
+BOT_DESC = package["description"]
+BOT_MAIN = package["main"]
+BOT_AUTHOR = package["author"]
+BOT_DEPENDENCIES = package["dependencies"]
 BOT_ID = 605836370749030490
 TESTING_ID = 607087546333265920
+OWNER_ID = 138461123899949057
 
-bot = commands.Bot(command_prefix='>')
+bot = commands.Bot(command_prefix='>', description = BOT_DESC, owner_id = OWNER_ID, activity = discord.Activity(name = "Listening Closely"))
 
 @bot.command()
 async def ping(channel):
@@ -27,6 +69,31 @@ async def ping(channel):
 async def takecaresteve(channel):
 	await channel.send('you too')
 
+@bot.command()
+async def seelist(channel, confirm=None):
+	#TODO are you sure?
+	print(data["filters"])
+	await channel.send(data["filters"])
+
+@bot.command()
+async def add(channel, keyword):
+	data["filters"].append(keyword)
+	with open("data.json", "w") as data_file:
+		json.dump(data, data_file)
+	str = "Added keyword {} to filter list".format(keyword)
+	print(str)
+	await channel.send(str)
+
+@bot.command()
+async def remove(channel, keyword):
+	try:
+		data["filters"].remove(keyword)
+		print("Successfully removed word from filter list.")
+		await channel.send("Successfully removed word from filter list.")
+	except ValueError:
+		print("Word not fould in filter list.")
+		await channel.send("Word not fould in filter list.")
+
 def is_not_me():
 	print("hello?")
 	def  predicate(ctx):
@@ -35,7 +102,14 @@ def is_not_me():
 	print("hi")
 	return commands.check(predicate)
 
-@bot.listen('on_message')
+@bot.command()
+async def shutdown(channel):
+	with open("data.json", "w") as data_file:
+		json.dump(data, data_file)
+	await bot.logout()
+	exit()
+
+#@bot.listen('on_message')
 @is_not_me()
 async def copy_message(msg):
 	if msg.author.bot: return
@@ -43,6 +117,37 @@ async def copy_message(msg):
 	if channel.id != TESTING_ID: return
 	await channel.send(msg.content)
 
+@bot.listen("on_message")
+async def filter_message(msg):
+	if msg.author.bot: return
+	channel = msg.channel
+	if channel.id != TESTING_ID: return
+	filterIndex = []
+	for filter in data["filters"]:
+		index = msg.content.upper().find(filter.upper())
+		if index > -1: filterIndex.append((index, index + len(filter)))
+	if len(filterIndex) > 0:
+		filterIndex.sort()
+		reply = generate_message(msg.content, filterIndex)
+		await channel.send(reply)
+
+
+
+#TODO make bot generate filtered message when a filter word is detected
+def generate_message(str, filterIndex):
+	new = ""
+	index = 0
+
+	for filter in filterIndex:
+		new = new + str[index:filter[0]] + "||" + str[filter[0]:filter[1]] + "||"
+		index = filter[1]
+		print(new)
+	new = new + str[index:]
+
+	print(new)
+
+	return new
 
 
 bot.run(auth_string)
+print("hello")
